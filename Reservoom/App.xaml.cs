8fc22163bc1814ab386d2d793f51cc32945eed00
Daniel PC <daniel.barczynski@onet.pl>
2022-06-27 20:43:53 +0200

    ex1 done

--- Reservoom/App.xaml.cs
+++ Reservoom/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Reservoom.Exceptions;
+using Reservoom.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +10,33 @@ using System.Windows;
 
 namespace Reservoom
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("SingletonSean Suites");
+
+            try
+            {
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 3),
+                    "SingletonSean",
+                    new DateTime(2000, 1, 1),
+                    new DateTime(2000, 1, 3)));
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 3),
+                    "SingletonSean",
+                    new DateTime(2000, 1, 1),
+                    new DateTime(2000, 1, 2)));
+            }
+            catch (ReservationConflictException ex)
+            {
+
+            }
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationsForUser("SingletonSean");
+            base.OnStartup(e);
+        }
     }
 }
---
+++ Reservoom/Models/Reservation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.Models
+{
+    public class Reservation
+    {
+        public string Username { get; }
+        public RoomID RoomID { get; }
+        public DateTime StartTime { get; }
+        public DateTime EndTime { get; }
+        public TimeSpan Length => EndTime.Subtract(StartTime);
+
+        public Reservation(RoomID roomID, string username, DateTime startTime, DateTime endTime)
+        {
+            RoomID = roomID;
+            StartTime = startTime;
+            EndTime = endTime;
+            Username = username;
+        }
+
+        internal bool Conflicts(Reservation reservation)
+        {
+            if (reservation.RoomID != RoomID)
+            {
+                return false;
+            }
+
+            return reservation.StartTime < EndTime && reservation.EndTime > StartTime; // existing vs incoming, it conflicts if someone made reservation on room where its EndTime "didn't finish"
+        }
+    }
+}
